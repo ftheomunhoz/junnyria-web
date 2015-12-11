01f65bed25c209195a5d48c736c6455530e19324
ftheomunhoz <ftheomunhoz@gmail.com>
2015-12-11 03:00:32 -0200

    changed port definition

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ var port = process.env.PORT || '9001';
 var host = '127.0.0.1';
 
 server.listen(port, host, function () {
-    console.log('Server started');
+    console.log('Listening on port %d', server_port);
 });
