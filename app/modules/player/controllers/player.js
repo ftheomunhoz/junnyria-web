756d39942d5f6d500fc64e26eb8a29b548c6cb8d
ftheomunhoz <ftheomunhoz@gmail.com>
2015-12-14 00:22:49 -0200

    player RWD - player tabs

--- app/modules/player/controllers/player.js
+++ app/modules/player/controllers/player.js
@@ -18,6 +18,24 @@
         vm.player = {
             avatar: '/assets/images/wizard.png'
         }
+
+        vm.panelFocusList = [
+            {
+                id: "panelLog",
+                title: "LOG DE ATIVIDADES"
+            },
+            {
+                id: "panelAttention",
+                title: "PONTO DE ATENÇÃO"
+            }
+        ];
+
+        vm.panelFocus = vm.panelFocusList[0];
+
+        vm.masterImage = {
+            large: '/assets/images/map.jpg',
+            small: '/assets/images/medusa.png'
+        }
     }
 
     angular.module('junnyria.player').controller("playerController", playerController);
