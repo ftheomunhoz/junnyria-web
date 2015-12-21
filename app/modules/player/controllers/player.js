db321e109d036886c7ca46293bf2f770eb967cb0
ftheomunhoz <ftheomunhoz@gmail.com>
2015-12-21 01:35:41 -0200

    panel player data layout

--- app/modules/player/controllers/player.js
+++ app/modules/player/controllers/player.js
@@ -23,6 +23,31 @@
             large: '/assets/images/map.jpg',
             small: '/assets/images/medusa.png'
         };
+
+        vm.panelList = [
+            {
+                title: 'GERAL'
+            },
+            {
+                title: 'ATRIBUTOS'
+            },
+            {
+                title: "HABILIDADES"
+            }
+        ];
+
+        vm.selectedTitle = "";
+
+        vm.togglePanel = function(list, index) {
+            for (var i=0 ; i<list.length ; i++) {
+                list[i].active = false;
+            }
+
+            list[index].active = true;
+            vm.selectedTitle = list[index].title;
+        };
+
+        vm.togglePanel(vm.panelList, 0);
     }
 
     angular.module('junnyria.player').controller("playerController", playerController);
