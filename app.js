468db0762eea38d965c25b7461ae732926fdae54
ftheomunhoz <ftheomunhoz@gmail.com>
2016-02-01 01:34:45 -0200

    debugging heroku

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ var app = express();
 var staticPath = path.resolve(path.join(__dirname, 'app'));
 app.use(express.static(staticPath));
 
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+
 app.use(function (req, res, next) {
-    if (process.env.NODE_ENV.toLowerCase() === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
+    if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
         return res.redirect('https://' + req.headers.host + req.url);
     } else {
         return next();
